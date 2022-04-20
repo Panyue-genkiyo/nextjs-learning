b40bd6d8c3d4e10bd8fad883443f82fcc592c7c6
panyue <90938238g@gmail.com>
2022-04-20 17:05:06 +0800

    / home页(注意Link会预加载指向页面所需的json数据)

--- next-pre-rendering/pages/index.js
+++ next-pre-rendering/pages/index.js
@@ -1,10 +1,19 @@
 import React from 'react';
+import Link from  'next/link';
 
 const Home = () => {
     return (
-        <h1>
-           next js pre-rendering
-        </h1>
+        <>
+            <h1>
+                next js pre-rendering
+            </h1>
+            <Link href='/users' passHref>
+                <p>users</p>
+            </Link>
+            <Link href='/posts' passHref>
+                <p>Posts</p>
+            </Link>
+        </>
     );
 };
 
