cac77028cae45031a2242a34d5104d810df17131
panyue <90938238g@gmail.com>
2022-04-23 16:19:42 +0800

    app layout in _app.js file

--- next-misc/pages/_app.js
+++ next-misc/pages/_app.js
@@ -1,7 +1,27 @@
-import '../styles/globals.css'
+import Head from "next/head";
+import Header from "@/layout/Header";
+import Footer from "@/layout/Footer";
+import 'styles/layout.css'
+import 'styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+
+    //自定义自己的样式
+    if(Component.getLayout){
+        return Component.getLayout(<Component {...pageProps} />);
+    }
+
+  return (
+      <>
+          <Head>
+              <title>py nextjs learning</title>
+              <meta name='description' content='next js learning'/>
+          </Head>
+          <Header/>
+            <Component {...pageProps} />
+          <Footer/>
+      </>
+  )
 }
 
 export default MyApp
